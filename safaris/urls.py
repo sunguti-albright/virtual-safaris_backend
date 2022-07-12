5bf5862ba49f0c6fde7de5f42caa676fd9d325a5
albright <sungutialbright@gmail.com>
2022-07-12 20:06:16 +0300

    fix url routes

--- safaris/urls.py
+++ safaris/urls.py
@@ -15,7 +15,8 @@ router.register(r'tourist', views.TouristViewSet, basename = 'tourist')
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+  
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
